e5243c5cd995303e5401d312f1bd8c56cd3871dc
Artem <artemermolaev0@gmail.com>
2021-03-23 09:03:45 +0200

    Context and services

---
+++ BusinessLogicLayer/UserService/UserService.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLogicLayer.Models;
+using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.UserService
+{
+    class UserService : IUserService
+    {
+		private readonly IApplicationDbContext _dbContext;
+		public UserService(IApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public List<UserDTO> GetAll()
+		{
+			var users = _dbContext.Users.ToList();
+			var userResult = new List<UserDTO>();
+
+			foreach (var user in users)
+			{
+				var mappedUser = new UserDTO { Login = user.Login, Password = user.Password };
+				userResult.Add(mappedUser);
+
+			}
+
+			return userResult;
+		}
+
+    }
+}
---
+++ DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
