06430e40b43f5381a2469a526ff93632b1e9049d
Artem <artemermolaev0@gmail.com>
2021-03-26 14:03:59 +0200

    private/public controllers

--- HomeWork1/ClientApp/src/app/app.module.ts
+++ HomeWork1/ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { LoginComponent } from './login/login.component';
+import { PrivateDataComponent } from './private-data/private-data.component';
+import { PublicDataComponent } from './public-data/public-data.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    LoginComponent,
+    PrivateDataComponent,
+    PublicDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +32,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'private-data', component: PrivateDataComponent },
+      { path: 'public-data', component: PublicDataComponent },
+      { path: 'login', component: LoginComponent },
     ])
   ],
   providers: [],
---
+++ HomeWork1/ClientApp/src/app/private-data/private-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-private-data',
+  templateUrl: './private-data.component.html',
+  styleUrls: ['./private-data.component.css']
+})
+export class PrivateDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
